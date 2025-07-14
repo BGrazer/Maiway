06fddfe7503609ad1c286c68a9a7c32d009e5b89
BGrazer <bravenreyes@gmail.com>
2025-07-14 19:59:08 +0800

    toml file add

--- maiwayapp/backend/main.py
+++ maiwayapp/backend/main.py
@@ -1,10 +1,14 @@
+from flask import Flask
 import subprocess
-import time
 
-subprocess.Popen(["python", "rfr.py"])
-subprocess.Popen(["python", "crowd_analysis.py"])
-subprocess.Popen(["python", "chatbot.py"])
+app = Flask(__name__)
 
-# Keep container alive
-while True:
-    time.sleep(10)
+@app.route('/')
+def hello():
+    return "Backend is running"
+
+if __name__ == "__main__":
+    subprocess.Popen(["python", "rfr.py"])
+    subprocess.Popen(["python", "crowd_analysis.py"])
+    subprocess.Popen(["python", "chatbot.py"])
+    app.run(host='0.0.0.0', port=5000)
